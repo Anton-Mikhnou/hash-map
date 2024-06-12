e46ac6c745918a61b75ec1cb183c7d6db04bf7fc
Anton Mikhnou <anton_mihnov03@mail.ru>
2024-06-12 17:53:43 +0300

    first version method set

--- modules/hash.js
+++ modules/hash.js
@@ -1,3 +1,5 @@
+import { log } from 'util';
+import LinkedList from './linkedList';
 import Node from './node';
 
 export default class HashMap {
@@ -5,10 +7,11 @@ export default class HashMap {
        this.buckets = new Array(16)
     }
 
-    hash(key) {
+    #hash(key) {
         let hashCode = 0;
       
         const primeNumber = 31;
+
         for (let i = 0; i < key.length; i++) {
             hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
         }
@@ -17,15 +20,22 @@ export default class HashMap {
     }
 
     set(key, value) {
-        let index = this.hash(key);
+        let index = this.#hash(key);
+        let pair = {};
+        pair[key] = value; 
 
-        if (this.buckets[index] !== '' && this.buckets[index] === value) {
-            this.buckets[index] = value;
+      
+        console.log(this.buckets[index] === '');
+
+        if (this.buckets[index] === '') {
+            let linkedList = new LinkedList();
+            this.buckets[index] = linkedList;
+            linkedList.append(pair);
         } else {
-            
-            this.buckets[index] = value;
+            let linkedList = new LinkedList();
+            this.buckets[index] = linkedList;
+            linkedList.append(pair);
         }
-
     }
 
     value(index) {
--- src/index.js
+++ src/index.js
@@ -3,46 +3,46 @@ import LinkedList from '../modules/linkedList';
 
 let hashMap = new HashMap();
 
-console.log(hashMap.set('Car', 'fsdf'));
+// console.log(hashMap.set('Car', 'fsdf'));
 
-hashMap.value(4);
+// hashMap.value('hh',4);
 
 console.log(hashMap.set('Car', 'trtr'));
 
 hashMap.value(4);
 
-hashMap.set('Mano', 'Farci')
+// hashMap.set('Mano', 'Farci')
 
-hashMap.value(5)
+// hashMap.value(5)
 
-hashMap.set('Mano', 'tff')
+// hashMap.set('Mano', 'tff')
 
-hashMap.value(5)
-console.log('===============');
-let list = new LinkedList();
+// hashMap.value(5)
+// console.log('===============');
+// let list = new LinkedList();
 
-console.log(list.append('sd'));
-console.log(list.append('tr'));
-// console.log(list.prepend('re'));
+// console.log(list.append('sd'));
+// console.log(list.append('tr'));
+// // console.log(list.prepend('re'));
 
-console.log(list.size());
-console.log('list', list);
+// console.log(list.size());
+// console.log('list', list);
 
 
-// console.log('head',list.getHead());
-// console.log('tail',list.getTail());
-// console.log(list.at(1));
-// console.log('list1', list);
+// // console.log('head',list.getHead());
+// // console.log('tail',list.getTail());
+// // console.log(list.at(1));
+// // console.log('list1', list);
 
-// console.log('pop',list.pop())
+// // console.log('pop',list.pop())
 
-console.log('list', list);
+// console.log('list', list);
 
-// list.append('pp');
-// list.append('rr')
-// list.pop()
-// list.pop()
+// // list.append('pp');
+// // list.append('rr')
+// // list.pop()
+// // list.pop()
 
-console.log('list', list);
-console.log('contains', list.contains('tr'));
+// console.log('list', list);
+// console.log('contains', list.contains('tr'));
 // console.log('find', list.find(''));
